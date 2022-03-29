ee1592d5dd8549902a8ce014afa72425d3119f7b
Sam Burton <samburton93@gmail.com>
2022-03-29 18:10:29 +0100

    kms

--- Assets/Scripts/ScriptA.cs
+++ Assets/Scripts/ScriptA.cs
@@ -7,7 +7,7 @@ public class ScriptA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.SetActive(false);
     }
 
     // Update is called once per frame
