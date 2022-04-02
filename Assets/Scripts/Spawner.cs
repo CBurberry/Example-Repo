1268f20822569b096e24b205014a53d42c9f4228
Sam Burton <samburton93@gmail.com>
2022-04-02 20:22:23 +0100

    Materials

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    List<PillSO> pillsTemplate = new List<PillSO>();
+    [SerializeField] List<PillSO> pillsTemplate = new List<PillSO>();
+
+    [SerializeField] List<Colour> colours = new List<Colour>();
+    [SerializeField] List<Shape> shapes = new List<Shape>();
+
+    [SerializeField] float range;
+
+
     void SpawnPill(PillSO pso)
     {
-        GameObject pill = Instantiate(pso.shape.prefab);
+        GameObject pill = Instantiate(pso.shape.prefab, this.transform);
         pill.GetComponent<MeshRenderer>().material = pso.colr.material;
     }
+
+    PillSO CreatePill()
+    {
+        PillSO newPill = ScriptableObject.CreateInstance<PillSO>();
+        newPill.colr = colours[Random.Range(0, colours.Count)];
+        newPill.shape = shapes[Random.Range(0, shapes.Count)];
+        return (newPill);
+    }
+
+
+
 
     // Update is called once per frame
     void Update()
@@ -18,6 +36,7 @@ public class Spawner : MonoBehaviour
     }    
     void Start()
     {
-        
+        PillSO tempSO = CreatePill();
+        SpawnPill(tempSO);
     }
 }
