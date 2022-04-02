fe53a22380e4cc6e05c6ba035f26133d30308c1a
Christian <christianburberry@gmail.com>
2022-04-02 16:55:43 +0100

    PatientChart UI control

---
+++ Assets/Scripts/UI/PatientChart.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using DG.Tweening;
+
+public enum UIState 
+{
+    Ready,
+    Busy
+};
+
+public class PatientChart : MonoBehaviour
+{
+    public bool IsShowing => gameObject.activeSelf;
+
+    [BoxGroup("Transitions")]
+    [SerializeField] Vector2 offScreenPosition = new Vector3(0f, -800f);
+    [BoxGroup("Transitions")]
+    [SerializeField] Vector2 visiblePosition = new Vector3(0f, 20f);
+    [BoxGroup("Transitions")]
+    [SerializeField] float showDuration = 1f;
+    [BoxGroup("Transitions")]
+    [SerializeField] float hideDuration = 1f;
+
+    RectTransform rect;
+    UIState state;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        state = UIState.Ready;
+    }
+
+    public void Toggle() 
+    {
+        if (IsShowing)
+        {
+            StartCoroutine(Hide());
+        }
+        else 
+        {
+            gameObject.SetActive(true);
+            StartCoroutine(Show());
+        }
+    }
+
+    private IEnumerator Show()
+    {
+        if (state == UIState.Busy) 
+        {
+            yield break;
+        }
+
+        state = UIState.Busy;
+        //Entry transition tween
+        yield return rect.DOAnchorPos(visiblePosition, showDuration).WaitForCompletion();
+        state = UIState.Ready;
+    }
+
+
+    private IEnumerator Hide() 
+    {
+        if (state == UIState.Busy)
+        {
+            yield break;
+        }
+
+        state = UIState.Busy;
+
+        //Exit transition tween
+        yield return rect.DOAnchorPos(offScreenPosition, hideDuration).WaitForCompletion();
+        gameObject.SetActive(false);
+        state = UIState.Ready;
+    }
+}
