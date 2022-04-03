2a643e0ead47cb31e7add0a17080488c16fe51e3
Christian <christianburberry@gmail.com>
2022-04-03 15:14:54 +0100

    Change namespacing of color such that it doesn't clash with unitys

--- Assets/Prefabs/Pills/Colours/Colour.cs
+++ Assets/Prefabs/Pills/Colours/Colour.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Color
-{
-    Red, 
-    Blue, 
-    Green, 
-    Yellow, 
-    White
-}
-
 [CreateAssetMenu(menuName = "Pills/Colour")]
 public class Colour : ScriptableObject
 {
+    public enum Color
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        White
+    };
+
     [SerializeField] public Material material;
     [SerializeField] Color colour;
 
